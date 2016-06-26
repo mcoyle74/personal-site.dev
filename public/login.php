a3879d0068242dcf2a3a9d395923421abeaeea25
Michael Coyle <mcoyle74@gmail.com>
2016-06-26 02:43:26 -0500

    All pages have been completely restyled--index.html now features a background-image, a photograph of a conceptual self-portrait. All other pages have background-color set to match the off white of this photo. The billiard ball logo has been replaced by "Michael David Coyle."

--- public/login.php
+++ public/login.php
@@ -16,46 +16,13 @@
 	<!-- Optional theme -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
 	<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
-	<style type="text/css">
-		body {
-			font-family: 'Montserrat', sans-serif;
-			background-color: #d6dad6;
-			padding-top: 10px;
-		}
-		.navbar-brand {
-			padding: 0 10px 0 0;
-			position: relative;
-			top: -7px;
-		}
-		.container-fluid {
-			padding-left: 10%;
-			padding-right: 10%;
-		}
-		a:hover {
-			transform: scale(1.1);
-		}	
-		#resume:hover {
-			color: #ff0c0c;
-		}
-		#portfolio:hover {
-			color: #bc79ff;
-		}
-		#contact:hover {
-			color: #4d88ff;
-		}
-		img.profile-pic {
-			width: 33%;
-			max-width: 350px;
-			margin: 20px 5% 5% 5%;
-			border-radius: 10%;
-			float: right;
-		}
-	</style>
+	
+	<link rel="stylesheet" type="text/css" href="/css/style-main.css">
 
 </head>
 <body>
-	<nav class="container-fluid navbar navbar-inverse navbar-collapse navbar-static-top">
-			<a class="navbar-brand" href="/index.html"><img id="brand" src="/img/4-ball-green.png" height="70"></a>
+	<nav class="container navbar navbar-static-top">
+			<a class="navbar-brand" href="/index.html">Michael David Coyle</a>
 		<ul class="nav navbar-nav">
 			<li><a href="/resume.html" id="resume">Resume</a></li>
 			<li><a href="/portfolio.php" id="portfolio">Portfolio</a></li>
@@ -63,7 +30,7 @@
 			<li><a href="/login.php" id="login">Sign in</a></li>
 		</ul>
 	</nav>
-	<div class="container-fluid">
+	<div class="container">
 		<h2>Login</h2>
 		<form method="POST" action="/login.php" role="form">
 			<div class="form-group">
--- public/portfolio.php
+++ public/portfolio.php
@@ -11,59 +11,14 @@
 	<!-- Optional theme -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
 	<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
-	<style type="text/css">
-		body {
-			font-family: 'Montserrat', sans-serif;
-			background-color: #d6dad6;
-			padding-top: 10px;
-		}
-		h1 {
-			padding-bottom: 50px;
-			transform: perspective(0, 10deg);
-		}
-		.navbar-brand {
-			padding: 0 10px 0 0;
-			position: relative;
-			top: -7px;
-		}
-		.container-fluid {
-			padding-left: 10%;
-			padding-right: 10%;
-		}
-		a:hover {
-			transform: scale(1.1);
-		} 
-		#resume:hover {
-			color: #ff0c0c;
-		}
-		#portfolio:hover {
-			color: #bc79ff;
-		}
-		#contact:hover {
-			color: #4d88ff;
-		}
-		img.img-web-thumb {
-			width: 25%;
-			max-width: 200px;
-			margin-bottom: 7%;
-			margin-left: 7%;
-			float: left;
-			transform: rotate3d(0, 1, 0, 30deg);
-			box-shadow: -20px 20px 30px;
-		}
-		#img-1 {
-			margin-left: 0;
-		}
-		.perspective {
-			perspective: 1200px;
-		}
-	</style>
+	
+	<link rel="stylesheet" type="text/css" href="/css/style-main.css">
 </head>
 
 <body>
-	<nav class="container-fluid navbar navbar-inverse navbar-static-top">
-		<a class="navbar-brand" href="/index.html"><img id="brand" src="/img/4-ball-green.png" height="70" alt="Green billiard ball"></a>
-		<ul class="nav navbar-nav navbar-left">
+	<nav class="container navbar navbar-static-top">
+		<a class="navbar-brand" href="/index.html">Michael David Coyle</a>
+		<ul class="nav navbar-nav">
 			<li><a href="/resume.html" id="resume">Resume</a></li>
 			<li><a href="portfolio.php" id="portfolio">Portfolio</a></li>
 			<li><a href="/contact.php" id="contact">Contact</a></li>
