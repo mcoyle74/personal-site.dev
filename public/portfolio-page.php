b844fd70cd6bb3b5a6f8fa2ff39bcc9eb7a6c240
Michael Coyle <mcoyle74@gmail.com>
2017-07-18 14:53:48 -0500

    Removed Bootstrap.

--- public/portfolio-page.php
+++ public/portfolio-page.php
@@ -5,51 +5,13 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	
-	<title>Norbert Clyde Martinez Jr.</title>
+	<title>Norbert Clyde Martinez, Jr.</title>
 
-	<!-- Latest compiled and minified CSS -->
-	<link 
-		rel="stylesheet" 
-		href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
-		integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" 
-		crossorigin="anonymous"
-	>
 	<link rel="stylesheet" type="text/css" href="/css/portfolio-page.css">
-
-	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
 </head>
 <body>
 
 <header>
-<nav class="navbar navbar-default hidden">
-	<div class="container">
-	<!-- Brand and toggle get grouped for better mobile display -->
-	<div class="navbar-header">
-		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1" aria-expanded="false">
-		<span class="sr-only">Toggle navigation</span>
-		<span class="icon-bar"></span>
-		<span class="icon-bar"></span>
-		<span class="icon-bar"></span>
-		</button>
-		<a class="navbar-brand" href="#">Norbert Clyde Martinez,&nbsp;Jr.</a>
-	</div>
-
-	<!-- Collect the nav links, forms, and other content for toggling -->
-	<div class="collapse navbar-collapse" id="navbar-collapse-1">
-		<ul class="nav navbar-nav navbar-right">
-			<li><a href="#">Work</a></li>
-			<li><a href="#">CV</a></li>
-			<li><a href="#">Contact</a></li>
-		</ul>
-	</div><!-- /.navbar-collapse -->
-	</div><!-- /.container -->
-</nav>
-
 <div class="page-header">
 	<h1>Norbert Clyde Martinez,&nbsp;Jr.</h1>
 	<ul>
@@ -72,14 +34,7 @@
 	<h1>Contact</h1>
 </section>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-
-<script 
-	src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" 
-	integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" 
-	crossorigin="anonymous"
->
-</script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 
 </body>
 </html>
